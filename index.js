75d2a1212ca25fd6f1d1ddd942083beedd57692b
Garcia, Josue <josue.garcia@icfnext.com>
2020-02-22 14:56:25 -0600

    Chapter 06

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = new express();
-const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const BlogPost = require('./models/BlogPost');
 
 mongoose.connect('mongodb://localhost/my_database', {
     useNewUrlParser: true
 });
 
-
-
-const getFilePath = file => `pages/${file}.html`;
-const getFile = name => path.resolve(__dirname, (getFilePath(name)));
-
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 
@@ -21,8 +21,11 @@ app.listen(4000, () => {
     console.log('App listening on port 4000');
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', {
+        blogposts
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -33,6 +36,18 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post', {
+        blogpost
+    });
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('create');
+});
+
+app.post('/posts/store', async (req, res) => {
+    await BlogPost.create(req.body);
+    res.redirect('/');
 });
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const defaultDate = new Date();
+
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: {
+        type: Date,
+        default: defaultDate
+    }
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
